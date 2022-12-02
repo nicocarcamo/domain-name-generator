9a6d8f822eb7fa5fedb1f5948d10afb73ee07312
nicocarcamo <nicolascarcamonaccarino@gmail.com>
2022-12-02 15:00:26 +0000

    URL generator project upload

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".cl"];
+
+document.getElementById("btn").addEventListener("click", function() {
+  let firstString = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let secondString = adj[Math.floor(Math.random() * adj.length)];
+  let thirdString = noun[Math.floor(Math.random() * noun.length)];
+  let fourthString = extension[Math.floor(Math.random() * extension.length)];
+  document.getElementById(
+    "url"
+  ).innerHTML = `${firstString}${secondString}${thirdString}${fourthString}`;
+});
